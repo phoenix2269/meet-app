41a84a96a43e912d28c22541880bade785ea0579
phoenix2269 <125770374+phoenix2269@users.noreply.github.com>
2023-09-09 18:26:27 -0400

    Final Commit for Exercise 4.10

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import { extractLocations, getEvents } from './api';
 import NumberofEvents from './components/NumberOfEvents';
-import './App.css';
+import CityEventsChart from './components/CityEventsChart';
+import EventGenresChart from './components/EventGenresChart';
 import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
+
+import './App.css';
 
 const App = () => {
     const [allLocations, setAllLocations] = useState([]);
@@ -39,25 +42,30 @@ const App = () => {
         setWarningAlert("You are currently offline");
     }
     fetchData();
+    // eslint-disable-next-line
   }, [currentCity, eventNumber]);
 
   return (
-      <div className="App">
-          <div className="alerts-container">
-              {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
-              {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
-              {warningAlert.length ? <WarningAlert text={warningAlert}/> : null}
-          </div>
-          <CitySearch
-              allLocations={allLocations}
-              setCurrentCity={setCurrentCity}
-              setInfoAlert={setInfoAlert}
-          />
-          <NumberofEvents
-              eventNumber={eventNumber}
-              onEventNumberChange={handleEventNumberChange}
-              setErrorAlert={setErrorAlert}
-            />
+    <div className="App">
+        <div className="alerts-container">
+            {infoAlert.length ? <InfoAlert text={infoAlert}/> : null}
+            {errorAlert.length ? <ErrorAlert text={errorAlert}/> : null}
+            {warningAlert.length ? <WarningAlert text={warningAlert}/> : null}
+        </div>
+        <CitySearch
+            allLocations={allLocations}
+            setCurrentCity={setCurrentCity}
+            setInfoAlert={setInfoAlert}
+        />
+        <NumberofEvents
+            eventNumber={eventNumber}
+            onEventNumberChange={handleEventNumberChange}
+            setErrorAlert={setErrorAlert}
+        />
+        <div className="charts-container">
+            <EventGenresChart events={events} />
+            <CityEventsChart allLocations={allLocations} events={events} />
+        </div>
         <EventList events={events} />
     </div>
   );
---
+++ src/components/EventGenresChart.js
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+import { ResponsiveContainer, PieChart, Pie, Cell, Legend, Tooltip } from "recharts";
+
+const EventGenresChart = ({ events }) => {
+    const [data, setData] = useState([]);
+    const genres = ["React", "JavaScript", "Node", "jQuery", "Angular"];
+    const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "#9d967c"];
+
+    useEffect(() => {
+        setData(getData());
+        // eslint-disable-next-line
+    }, [`${events}`]);
+
+    const getData = () => {
+        const data = genres.map((genre) => {
+            const filteredEvents = events.filter(event => event.summary.includes(genre));
+            return {
+                name: genre,
+                value: filteredEvents.length
+            };
+        });
+        return data;
+    };
+
+    const renderCustomizedLabel = ({
+        cx,
+        cy,
+        midAngle,
+        outerRadius,
+        percent,
+        index,
+    }) => {
+        const RADIAN = Math.PI / 180;
+        const radius = outerRadius;
+        const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
+        const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07;
+        return percent ? (
+            <text
+                x={x}
+                y={y}
+                fill="#black"
+                textAnchor={x > cx ? "start" : "end"}
+                dominantBaseline="central"
+            >
+                {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
+            </text>
+        ) : null;
+    };
+
+    return (
+        <ResponsiveContainer width="99%" height={400}>
+        <PieChart>
+            <Pie
+                data={data}
+                dataKey="value"
+                fill="#8884d8"
+                labelLine={false}
+                label={renderCustomizedLabel}
+                outerRadius={150}
+            >
+            {data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={colors[index]} />
+            ))}
+            </Pie>
+            <Legend verticalAlign="bottom" height={36} iconType="plainline" />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} contentStyle={{color: "black", backgroundColor: "#7C9D96", borderRadius: "10px", display: "flex", justifyContent: "center", border: "none", padding: "3px"}}/>
+        </PieChart>
+        </ResponsiveContainer>
+    );                    
+};
+
+export default EventGenresChart;
